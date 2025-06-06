4d2d47c1852ed28cc0e75642b8a06b92fb004fdd
riokuchlyan <riokuchlyan@gmail.com>
2025-06-06 10:47:18 -0400

    Remove debug folder

--- src/app/userdata/page.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@/utils/supabase/server';
-
-export default async function UserData() {
-  const supabase = await createClient();
-  const { data: userData } = await supabase.from("userdata").select();
-
-  return <pre>{JSON.stringify(userData, null, 2)}</pre>
-}
