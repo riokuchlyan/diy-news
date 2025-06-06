8dcee37aada9fd34f79c7d1fd7d3452144ec02d1
riokuchlyan <riokuchlyan@gmail.com>
2025-06-06 14:18:09 -0400

    Change test page

--- src/app/testdogcat/page.tsx
+++ src/app/testdogcat/page.tsx
@@ -1,12 +1,11 @@
-//import { sendNewsletter } from "@/services/news-letter/NewsLetter";
-import { getParsedNews } from "@/utils/getParsedNews";
+import { sendNewsletter } from "@/services/news-letter/NewsLetter";
+
 
 export default async function Test() {
     try {
-        const parsedNews = await getParsedNews("education");
-        return <div>{parsedNews}</div>;
+        await sendNewsletter()
+        return <div>Sent newsletter</div>;
     } catch (error) {
-        console.error('Error in test page:', error);
         return <div>Error loading news: {error instanceof Error ? error.message : 'Unknown error'}</div>;
     }
 }
