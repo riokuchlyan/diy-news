b61c53e4e767389202e51f18a4f07b76d45f2560
riokuchlyan <rio.kuchlyan@riokuchlyan.com>
2025-08-05 17:49:44 -0400

    Bug fix

--- src/app/api/newsletter/route.ts
+++ src/app/api/newsletter/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { sendNewsletter } from '@/services/news-letter/NewsLetter';
-//import { sendNewsletterToTestUser } from '@/services/news-letter/NewsLetter'; // for testing
+//import { sendNewsletterToTestUser } from '@/services/news-letter/NewsLetter'; // for testing 
 
 export async function POST(request: Request) {
   try {
